4914f993fa8d6e5d779b182a8349f067cd1cf20f
Yosf96633 <yosf96633@gmail.com>
2025-03-24 18:06:07 +0500

    Contact updated

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,109 +1,22 @@
 "use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { GithubIcon, Linkedin } from "lucide-react";
-
-const formSchema = z.object({
-  name: z.string().min(2, {
-    message: "Name must be at least 2 characters.",
-  }),
-  email: z.string().email("Please enter valid email"),
-  message: z.string().min(2, {
-    message: "message must be at least 2 characters.",
-  }),
-});
-
+import { GithubIcon, LinkedinIcon, MailIcon } from "lucide-react";
 export function ProfileForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      email: "",
-      message: "",
-    },
-  });
-  const onSubmit = async () => {};
   return (
     <div id="contact" className=" bg-[#0A0A0A] flex flex-col items-center">
       <h1 className=" text-white text-center md:py-12 md:text-5xl md:font-semibold py-6 text-2xl font-normal">
         Contact me!
       </h1>
-      <div className=" flex flex-col pb-8 items-center justify-center">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className=" text-white">Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      className=" w-[70vw] text-white md:w-[30vw] py-5"
-                      placeholder="John Doe"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className=" text-white">Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="  w-[70vw] text-white  md:w-[30vw] py-5"
-                      placeholder="john@example.com"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="message"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className=" text-white">Message</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="  w-[70vw] text-white md:w-[30vw] py-5"
-                      placeholder="Hi"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className=" flex justify-center">
-              <Button className=" bg-white text-black" type="submit">
-                Submit
-              </Button>
-            </div>
-          </form>
-        </Form>
-      </div>
+    <div className=" flex flex-wrap py-6 space-x-8 justify-center items-center">
+    <a href="https://github.com/Yosf96633" target="_blank" rel="noopener noreferrer">
+              <GithubIcon className=" text-white" size={28}/>
+            </a>
+            <a href="www.linkedin.com/in/m-yousaf-b357172b6" target="_blank" rel="noopener noreferrer">
+              <LinkedinIcon className=" text-white" size={28}/>
+            </a>
+            <a href="mailto:yosf96633@gmail.com">
+              <MailIcon className=" text-white" size={28}/>
+              </a>
+    </div>
       </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,21 +2,9 @@ import { GithubIcon, LinkedinIcon, MailIcon } from "lucide-react";
 
 const Footer = () => {
     return (
-      <footer className="bg-[#0A0A0A] text-white py-6">
-        <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
+      <footer id="contact" className="bg-[#0A0A0A] text-white py-6">
+        <div className="container mx-auto px-6 flex flex-col md:flex-row justify-center items-center">
           <p className="text-sm">&copy; {new Date().getFullYear()} All rights reserved.</p>
-  
-          <div className="flex space-x-4 mt-4 md:mt-0">
-            <a href="https://github.com/Yosf96633" target="_blank" rel="noopener noreferrer">
-              <GithubIcon className=" text-white" size={28}/>
-            </a>
-            <a href="www.linkedin.com/in/m-yousaf-b357172b6" target="_blank" rel="noopener noreferrer">
-              <LinkedinIcon className=" text-white" size={28}/>
-            </a>
-            <a href="mailto:yosf96633@gmail.com">
-              <MailIcon className=" text-white" size={28}/>
-            </a>
-          </div>
         </div>
       </footer>
     );
